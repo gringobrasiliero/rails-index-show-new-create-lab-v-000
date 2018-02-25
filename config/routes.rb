e36e5fb0a709dcf672e81ea8b295a3a92773285b
Nolan Byrnes <nolanehs@gmail.com>
2018-02-25 15:58:38 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-resources :coupons, only: [:index, :new, :create]
+resources :coupon, only: [:index, :new, :create]
 
 # get '/coupon/:id', to: 'coupon#show', as: 'coupons'
   # The priority is based upon order of creation: first created -> highest priority.
